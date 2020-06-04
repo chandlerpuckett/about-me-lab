21edc4396253d2977d5f519c9aab89d1f2d261d8
David Quinn <david.c.quinn25@gmail.com>
2020-06-04 16:28:42 -0700

    made all changes

--- js/app.js
+++ js/app.js
@@ -47,148 +47,146 @@
 
 
 
-// ========== GLOBAL VAR + GREETING ========== //
-
-var arrayTally = [];
+//========== GLOBAL VAR + GREETING ========== //
+var score = 0
 var userName = prompt('What\'s your name?');
 
 alert('Howdy ' + userName + '!' + ' Nice to meet you. I\'m Chandler. here\'s a little bit of info about me!');
-
 // ========== Question 1 ========== //
 
-
-var answer1 = prompt('Am I from South Carolina?') .toLowerCase() .replace('yes','y');
-if (answer1 === 'y') {
-  alert('DING DING DING! great job! ' + userName + '! I\'m from Greenwood, South Carolina!');
-  arrayTally.push(1);
-} else {
-  alert('Nope! I\'m originally from Greenwood, South Carolina!');
-  arrayTally.push(0);
+function homeTown(){
+  var answer1 = prompt('Am I from South Carolina?') .toLowerCase() .replace('yes','y');
+  if (answer1 === 'y') {
+    score ++;
+    alert('DING DING DING! great job! ' + userName + '! I\'m from Greenwood, South Carolina!');
+  } else {
+    alert('Nope! I\'m originally from Greenwood, South Carolina!');
+  }
 }
 
+homeTown();
 
 // ========== Question 2 ========== //
 
-
-var answer2 = prompt('Have I ever spent more than one month in the woods?') .toLowerCase() .replace('yes','y');
-if (answer2 === 'y') {
-  alert('DING DING DING! I spent two months in the woods of north georgia taking part in a wilderness program');
-  arrayTally.push(1);
-} else {
-  alert('NOPE! I spent two months in the woods of north georgia taking part in a wilderness program');
-  arrayTally.push(0);
+function woods(){
+  var answer2 = prompt('Have I ever spent more than one month in the woods?') .toLowerCase() .replace('yes','y');
+  if (answer2 === 'y'){ 
+    score ++;
+    alert('DING DING DING! I spent two months in the woods of north georgia taking part in a wilderness program');
+  } else {
+    alert('NOPE! I spent two months in the woods of north georgia taking part in a wilderness program');
+    
+  }
 }
 
-
+woods();
 // ========== Question 3 ========== //
 
-
-var answer3 = prompt('Am I bilingual?') .toLowerCase() .replace('no','n');
-if (answer3 === 'n'){
-  alert('DING DING DING! Just english. I\'ve tried to teach myself French but never stuck with it long enough to learn.');
-  arrayTally.push(1);
-} else {
-  alert('NOPE. Just english');
-  arrayTally.push(0);
+function bilingual(){
+  var answer3 = prompt('Am I bilingual?') .toLowerCase() .replace('no','n');
+  if (answer3 === 'n'){
+    score ++;
+    alert('DING DING DING! Just english. I\'ve tried to teach myself French but never stuck with it long enough to learn.');
+  } else {
+    alert('NOPE. Just english');
+    
+  }
 }
+bilingual();
+//========== Question 4 ========== //
 
-
-// ========== Question 4 ========== //
-
-
-var answer4 = prompt('Am I a morning person?') .toLowerCase() .replace('no', 'n');
-if (answer4 === 'n'){
-  alert('DING DING DING! hell to the nah. I love to sleep');
-  arrayTally.push(1);
-} else {
-  alert('hell to the nah. I love to sleep');
-  arrayTally.push(0);
+function morning(){
+  var answer4 = prompt('Am I a morning person?') .toLowerCase() .replace('no', 'n');
+  if (answer4 === 'n'){
+    score ++;
+    alert('DING DING DING! hell to the nah. I love to sleep');
+  } else {
+    alert('hell to the nah. I love to sleep');
+    
+  }
 }
-
-
+morning();
 // ========== Question 5 ========== //
 
-
-var answer5 = prompt('Do aliens exist?') .toLowerCase() .replace('yes', 'y');
-if (answer5 === 'y'){
-  alert(' 👽 ' + 'I WANT TO BELIEVE ' + ' 👽' );
-  arrayTally.push(1);
-} else {
-  alert(' 👽👽👽👽👽👽👽👽👽👽👽 ');
-  arrayTally.push(0);
-}
-
-
-// ========== Question 6 ========== //
-
-var tries = 4;
-
-do {
-  var guess = prompt('How old am I? Hint: guess a number between 25 and 35');
-  var userGuess = parseInt(guess);
-
-  tries--;
-
-  if (userGuess >= 31) {
-    alert('NOPE, lower. ' + tries + ' tries left.');
-  } else if (userGuess <= 29) {
-    alert('NOPE, higher. ' + tries + ' tries left.');
-  } else if (userGuess === 30) {
-    arrayTally.push(1);
-    alert('Great job ' + userName + '!');
-    break;
-  }
-
-} while (tries > 0);
-
-if (tries === -1){
-  arrayTally.push(0);
-  alert('No more tries. I\'m 30');
-
-}
-
-// ========== Question 7 ========== //
-
-var tries2 = 6;
-var cityArray = ['london','daytona beach','berlin','vancouver','hay\'s kansas','orlando','montreal'];
-var cityAnswers = cityArray.toString(); // quicker way, but format issues?
-
-console.log(cityGuess);
-
-do {
-  var cityGuess = prompt('What\'s my favorite city to visit? ' + cityArray[0] + ', ' + cityArray[1]+', '+cityArray[2]+', '+cityArray[3]+', '+cityArray[4]+', '+cityArray[5]+', or '+cityArray[6]+'?').toLowerCase();
-
-  console.log(cityGuess);
-
-  tries2--;
-
-  if (cityGuess === cityArray[6]){
-    alert('Great job! I love Montreal!');
-    arrayTally.push(1);
-    break;
-
+function aliens(){
+  var answer5 = prompt('Do aliens exist?') .toLowerCase() .replace('yes', 'y');
+  if (answer5 === 'y'){
+    score ++;
+    alert(' 👽 ' + 'I WANT TO BELIEVE ' + ' 👽' );
   } else {
-    alert('NOPE! ' + tries2 + ' Tries left. Hint: ' + cityAnswers);
+    alert(' 👽👽👽👽👽👽👽👽👽👽👽 ');
+    
   }
+}
+
+aliens();
+// ========== Question 6 ========== //
+function ageQuestion(){
+  var tries = 4;
+
+  do {
+    var guess = prompt('How old am I? Hint: guess a number between 25 and 35');
+    var userGuess = parseInt(guess);
+
+    tries--;
+
+    if (userGuess >= 31) {
+      alert('NOPE, lower. ' + tries + ' tries left.');
+    } else if (userGuess <= 29) {
+      alert('NOPE, higher. ' + tries + ' tries left.');
+    } else if (userGuess === 30) {
+      alert('Great job ' + userName + '!');
+      score ++;
+      break;
+    }
+
+  } while (tries > 0);
+
+  if (tries === -1){
+    
+    alert('No more tries. I\'m 30');
+
+  }
+}
+ageQuestion();
+//========== Question 7 ========== //
+function favoriteCity(){
+  var tries2 = 6;
+  var cityArray = ['london','daytona beach','berlin','vancouver','hay\'s kansas','orlando','montreal'];
+  var cityAnswers = cityArray.toString(); // quicker way, but format issues?
 
   console.log(cityGuess);
 
-} while (tries2 > 0);
+  do {
+    var cityGuess = prompt('What\'s my favorite city to visit? ' + cityArray[0] + ', ' + cityArray[1]+', '+cityArray[2]+', '+cityArray[3]+', '+cityArray[4]+', '+cityArray[5]+', or '+cityArray[6]+'?').toLowerCase();
 
-if (tries2 === 0){
-  alert('No more tries. My favorite city to visit is Montreal!');
-  arrayTally.push(0);
+    console.log(cityGuess);
+
+    tries2--;
+
+    if (cityGuess === cityArray[6] || cityGuess === cityArray[3]){
+      alert('Great job! I love Montreal!');
+      score ++;
+      break;
+
+    } else {
+      alert('NOPE! ' + tries2 + ' Tries left. Hint: ' + cityAnswers);
+    }
+
+    console.log(cityGuess);
+
+  } while (tries2 > 0);
+
+  if (tries2 === 0){
+    alert('No more tries. My favorite city to visit is Montreal!');
+    
+  }
 }
+favoriteCity ();
 
 // ========== FINAL SCORE ========== //
 
-var sum = 0;
-
-for (var i = 0; i < arrayTally.length; i++){
-  var tally = sum += arrayTally[i];
-}
-
-alert('Congrats ' + userName + ' you got ' + tally + ' out of 7 questions correct');
+alert('Congrats ' + userName + ' you got ' + score + ' out of 7 questions correct');
 
 
 
